fc99431beb3b582cc315a189d4d21e18ffd11598
riddlework <82472185+riddlework@users.noreply.github.com>
2023-10-09 14:24:44 -0700

    NEW GUI

---
+++ TreeGUI/Program.fs
@@ -0,0 +1,41 @@
+﻿namespace TreeGUI
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Input
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "TreeGUI"
+        base.Width <- 1000.0
+        base.Height <- 1000.0
+        this.Content <- Counter.view
+
+        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+
+        
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme(baseUri = null, Mode = FluentThemeMode.Light)) // Might cause an error?
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
