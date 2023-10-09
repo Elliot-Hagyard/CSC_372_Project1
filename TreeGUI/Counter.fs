d67ecfab67b5b4774bc8b5b0b8510de52a34f642
riddlework <82472185+riddlework@users.noreply.github.com>
2023-10-09 16:00:37 -0700

    createText and createRect

--- TreeGUI/Counter.fs
+++ TreeGUI/Counter.fs
@@ -13,77 +13,69 @@ module TreeViewer =
 
     open Avalonia.Media
     open Avalonia.Controls.Shapes
+
     type point = {
         x : float
         y : float
     }
+
     let createText text point = 
         TextBlock.create [
-            TextBlock.width 64
-            TextBlock.horizontalAlignment HorizontalAlignment.Center
-            TextBlock.verticalAlignment VerticalAlignment.Top
-            //TODO
-            TextBlock.text $"{t.value}"
-        ]
-                        
-    let createRect rot point = 
-            Rectangle.create [
-                Rectangle.width 5.0
-                Rectangle.height 100.0
-                Rectangle.fill (SolidColorBrush (Color.Parse "black"))
-                
-                Rectangle.renderTransform (
-                    RotateTransform 45.0
-                )
+                TextBlock.width 64
+                TextBlock.horizontalAlignment HorizontalAlignment.Center // Is this necessary?
+                // TextBlock.verticalAlignment // Is this necessary?
+                TextBlock.text text
 
-                Canvas.left x
-                Canvas.top y
+                Canvas.left point.x
+                Canvas.top point.y
             ]
-    // Point is the current point
-    let rec drawTree tree point =
-        match tree with
-        | Empty -> []
-        | Tree t -> 
-            match (t.left, t.right) with
-            | (Empty, Empty) -> [createText $"{t.value}" x y]
-            | (_, Empty) -> [createText $"{t.value}" createRect x y 45.0;]@drawTree t.left@drawTree t.right]
-            | (Empty, _) -> [createText ${t.value}; createRect point{x=x; y=y} -45.0]@drawTree t.left@drawTree t.right]
-            | (_, _) -> [createText ${t.value}; createRect x y 45.0; createRect x y -45.0]@drawTree t.left@drawTree t.right]
+    
+    let createRect rot point =
+        // rot is 45.0 or -45.0
+        // alignment?
+        Rectangle.create [
+            Rectangle.width 2.5
+            Rectangle.height 50.0
+            Rectangle.fill (SolidColorBrush (Color.Parse "black"))
+            Rectangle.renderTransform(RotateTransform rot)
 
-    let view =
+            Canvas.left point.x
+            Canvas.top point.y
+            
+        ]
+            
+    // point is the xy-coords of current node
+    // let rec drawTree tree point = 
+    //     match tree with
+    //     | Empty -> []
+    //     | Tree t -> 
+    //         match (t.left, t.right) with
+    //         | (Empty, Empty) -> [createText $"{t.value}" x y]
+    //         | (_, Empty) -> [createText $"{t.value}" createRect x y 45.0;]@drawTree t.left@drawTree t.right]
+    //         | (Empty, _) -> [createText ${t.value}; createRect point{x=x; y=y} -45.0]@drawTree t.left@drawTree t.right]
+    //         | (_, _) -> [createText ${t.value}; createRect x y 45.0; createRect x y -45.0]@drawTree t.left@drawTree t.right]
+
+    let view tree =
         Component(fun ctx ->
             let state = ctx.useState 0
             DockPanel.create [
                 DockPanel.verticalAlignment VerticalAlignment.Top
                 DockPanel.horizontalAlignment HorizontalAlignment.Center
                 DockPanel.children [
-                    TextBlock.create [
-                        TextBlock.width 64
-                        TextBlock.horizontalAlignment HorizontalAlignment.Center
-                        TextBlock.verticalAlignment VerticalAlignment.Top
-                        TextBlock.text "Test"
-                    ]
-                    Canvas.create [
-                        Canvas.height 100.00
-                        Canvas.background Brushes.Transparent
-                        Canvas.children [
-                            Rectangle.create [
-                                Rectangle.width 5.0
-                                Rectangle.height 100.0
-                                Rectangle.fill (SolidColorBrush (Color.Parse "black"))
-                                
-                                Rectangle.renderTransform (
-                                    RotateTransform 45.0
-                                )
+                    
+                    Canvas.create [ // Remember to initialize position
 
-                                Canvas.left 20.0
-                                Canvas.top 20.0
-                            ]
-                        ]
-                    ]
+                        Canvas.height 400.0
+                        Canvas.background Brushes.Transparent
+                        Canvas.children [drawTree tree]
+
                 ]
 
-                // DockPanel.children [
+            ]
+        )
+
+
+ // DockPanel.children [
                 //     Button.create [
                 //         Button.width 64
                 //         Button.horizontalAlignment HorizontalAlignment.Center
@@ -115,5 +107,3 @@ module TreeViewer =
                 //         TextBlock.text (string state.Current)
                 //     ]
                 // ]
-            ]
-        )
